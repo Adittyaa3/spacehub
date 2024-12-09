bbcab5f07980c6dd338d973d0b5feec3104213f0
Aadityaa3 <adityavalo333@gmail.com>
2024-12-09 09:29:49 +0700

    update payment 1

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'server_key' => env('MIDTRANS_SERVER_KEY', 'SB-Mid-server-Zzi7Xi3SWGZPfuhfswePeIWL'),
-    'client_key' => env('MIDTRANS_CLIENT_KEY', 'SB-Mid-client-rCuKgpQHAxPcqVU_'),
+    'server_key' => env('MIDTRANS_SERVER_KEY'),
+    'client_key' => env('MIDTRANS_CLIENT_KEY'),
     'is_production' => false,
     'is_sanitized' => true,
     'is_3ds' => true,
