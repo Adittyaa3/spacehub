0432a4d66f3077d88e2ab209e3f054dbb1d0492a
evika <pitalokavk@gmail.com>
2024-12-18 11:05:04 +0700

    setting menu berhasil

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="container">
     <h1 class="my-4">Menu Role Settings</h1>
-    
+
     <!-- Form to update role-menu settings -->
     <form action="{{ route('settings.update') }}" method="POST">
         @csrf
@@ -18,13 +18,15 @@
                         </div>
                         <div class="card-body">
                             @foreach($menus as $menu)
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="role_menu[{{ $role->id }}][]" value="{{ $menu->id }}"
-                                           @if(in_array($menu->id, $roleMenus[$role->id]->pluck('menu_id')->toArray())) checked @endif>
-                                    <label class="form-check-label" for="role_menu_{{ $role->id }}_{{ $menu->id }}">
-                                        {{ $menu->name }}
-                                    </label>
-                                </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox"
+                                       name="role_menu[{{ $role->id }}][]"
+                                       value="{{ $menu->id }}"
+                                       @if(isset($roleMenus[$role->id]) && $roleMenus[$role->id]->contains('menu_id', $menu->id)) checked @endif>
+                                <label class="form-check-label" for="role_menu_{{ $role->id }}_{{ $menu->id }}">
+                                    {{ $menu->name }}
+                                </label>
+                            </div>
                             @endforeach
                         </div>
                     </div>
